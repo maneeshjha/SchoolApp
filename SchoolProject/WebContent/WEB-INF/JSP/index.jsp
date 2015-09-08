8a3cc2d193c89195c289a0de02b97a608ce83c6f
maneeshjha <maneesh08.jha@gmail.com>
2015-09-08 23:24:18 +0530

    Initial commit

---
+++ SchoolProject/WebContent/WEB-INF/JSP/index.jsp
@@ -0,0 +1,16 @@
+<html>
+<head>
+<title>Spring MVC Tutorial Series by Crunchify.com</title>
+</head>
+<body>
+	<br>
+	<div style="text-align:center">
+		<h2>
+			Hey You..!! This is your 1st Spring MCV Tutorial..<br> <br>
+		</h2>
+		<h3>
+			Hi!!!
+		</h3>
+	</div>
+</body>
+</html>
---
+++ SchoolProject/src/com/slokh/controller/HelloWorldContainer.java
@@ -0,0 +1,33 @@
+package com.slokh.controller;
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.Map;
+
+import org.joda.time.LocalDate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+@Controller
+@RequestMapping("/")
+public class HelloWorldContainer {
+ 
+    @RequestMapping(method = RequestMethod.GET)
+    public String sayHello(ModelMap model) {
+        model.addAttribute("greeting", "Hello World from Spring 4 MVC");
+        return "welcome";
+    }
+ 
+    @RequestMapping(value="/helloagain", method = RequestMethod.GET)
+    public String sayHelloAgain(ModelMap model) {
+        model.addAttribute("greeting", "Hello World Again, from Spring 4 MVC");
+        return "welcome";
+    }
+ 
+}
